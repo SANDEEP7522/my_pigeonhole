690b327961f31b7ef0cf935709d29485b65e6fd9
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-21 17:56:30 +0530

    Added ABOUT  section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Button } from '@/components/ui/button';
 import Navbar from './components/NaveBar/Navbar';
 import Hero from './components/Hero/Hero';
+import About from './components/About/About';
 function App() {
 
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
