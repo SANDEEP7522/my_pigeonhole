0d8d82d19d578cea82abe8c6a24153a761b266d9
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-01-21 17:12:39 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 
 import { Button } from '@/components/ui/button';
+import Navbar from './components/NaveBar/Navbar';
 function App() {
 
   return (
     <div>
-      <Button>Click me</Button>
+      <Navbar />
     </div>
   );
 }
